366a658f08fc8016ce9a3c795167533389a090d2
Filip Bengs <filip.bengs@netikka.fi>
2020-10-27 16:34:45 +0200

    updated files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import Navigation from './components/Navigation/Navigation.js'
-import Logo from './components/Logo/Logo.js'
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js'
+import Navigation from './components/Navigation/Navigation.js';
+import Logo from './components/Logo/Logo.js';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './components/Rank/Rank.js';
+import Particles from 'react-particles-js';
 
+const particleOptions = {
+  "particles": {
+      "number": {
+          "value": 150
+      },
+      "size": {
+          "value": 1
+      },
+      "color": {
+        "value": "#fff"
+      },
+      "line_linked": {
+        color: '494949',
+        opacity: 0.7
+      }
+  }
+}
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      //input will store the users input
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event);
+  }
+render(){  
   return (
     <div className="App">
-      <Navigation />
+      <Particles className='particles'
+        params={particleOptions}
+      />
+      <Navigation/>
       <Logo/>
-      <ImageLinkForm/>
+      <Rank/>
+      <ImageLinkForm onInputChange={this.onInputChange}/>
       {/*<FaceRecognition/>*/}
     </div>
   );
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange}) => {
     return (
         <div className="container">
             <p className='f2 white-80'>
                 {'This Magic Brain will detect faces in your pictures. Give it a try!'}
             </p>
-            <div className='center form pa4 br3 shadow-5'>
-                <input className='f4 pa2 w-70 center b--none' type='text' placeholder='enter a url'/>
+            <div className='center form'>
+                <input className='f4 pa2 w-70 center b--none' type='text' placeholder='enter a url' onChange={onInputChange}/>
                 <button className='w-30 f4 link ph3 pv2 dib bg-yellow dark-gray b--none'>detect</button>
             </div>
         </div>
     );
 }
-
 
 export default ImageLinkForm;
